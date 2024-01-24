40c6c598d3ed795f8b4af8a0ac7fe4705f549f0e
Sanchit <tech@bharatrohan.in>
2024-01-24 11:51:29 +0530

    Removed old files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import './App.css';
-import Home from "./components/Home";
-import FarmerProfile from "./components/FarmerProfile";
-import Login from "./components/Login";
 import ProtectedRoute from "./ProtectedRoute";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -13,9 +10,6 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/farmerprofile/:paramsone" element={<ProtectedRoute><FarmerProfile /></ProtectedRoute>} />
-          <Route path="/" element={<Login />} />
         </Routes>
       </Router>
       <ToastContainer />
